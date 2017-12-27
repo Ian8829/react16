d678526617551292943b3d2acd3dc3071823329b
taeshik Hwang <ghjkghj@naver.com>
2017-12-27 16:35:14 +0900

    Working with Props 1

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className='App'>
-    //     <h1>Hello...</h1>
-    //   </div>
-    // );
-
-    return React.createElement(
-      'div', {className: 'App'}, React.createElement('h1', null, 'Hello...')
-    )
+    return (
+      <div className='App'>
+        <h1>Hello...</h1>
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29">My hobbies?</Person>
+        <Person name="Stephanie" age="26" />
+      </div>
+    );
   }
 }
 
